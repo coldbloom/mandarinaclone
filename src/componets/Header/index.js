4abffc30f7b8f2661cc8cb912ade5e580bf09fdc
unknown <coldbloom22@gmail.com>
2023-02-06 00:48:00 +0300

    first commit

---
+++ src/componets/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='flex justify-between'>
+            <p className='text-blue'>Mandarina</p>
+            <div>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
