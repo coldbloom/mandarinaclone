3f5b7ef09a7fb7611a4b04ffbbb3caab0d738e50
unknown <coldbloom22@gmail.com>
2023-02-17 00:55:44 +0300

    desktop 2 0 55

--- src/componets/PopularDestinations/DesktopDestinations.js
+++ src/componets/PopularDestinations/DesktopDestinations.js
@@ -25,9 +25,22 @@ const DesktopDestinations = () => {
                                 </div>
                             </div>
                             <div className='col-7'>
-                                <div className='popular_card card_bg_GR' style={{backgroundImage: `url(${grece})`}}>
+                                <div className='popular_card card_bg_GR' style={{backgroundImage: `url(${grece})`, height: "100%"}}>
                                     <div className="title_popular_card">Греция</div>
                                     <div className="description_popular_card">59 гостинницы</div>
+                                </div>
+                            </div>
+                            <div className='col-12 col-lg-5  d-flex flex-column justify-content-between'>
+                                <div className='row'>
+                                    <div className='col-12 d-flex flex-column justify-content-between'>
+                                        <div className='popular_card card_bg_TR card_size-3 mb-card-country' style={{backgroundImage: `url(${turkey})`}}>
+                                            <div className="title_popular_card">Турция</div>
+                                            <div className="description_popular_card">36 гостиницы</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='row'>
+                                    <div className=''></div>
                                 </div>
                             </div>
                         </div>
