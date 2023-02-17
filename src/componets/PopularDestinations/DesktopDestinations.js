a19fff316e6ce43095cd4c22f64e991bf8f920f1
Konstantin.Kolesnik <kostya_kol333@mail.ru>
2023-02-17 08:51:07 +0300

    популярные направления done

--- src/componets/PopularDestinations/DesktopDestinations.js
+++ src/componets/PopularDestinations/DesktopDestinations.js
@@ -30,22 +30,26 @@ const DesktopDestinations = () => {
                                     <div className="description_popular_card">59 гостинницы</div>
                                 </div>
                             </div>
-                            <div className='col-12 col-lg-5  d-flex flex-column justify-content-between'>
-                                <div className='row'>
-                                    <div className='col-12 d-flex flex-column justify-content-between'>
-                                        <div className='popular_card card_bg_TR card_size-3 mb-card-country' style={{backgroundImage: `url(${turkey})`}}>
-                                            <div className="title_popular_card">Турция</div>
-                                            <div className="description_popular_card">36 гостиницы</div>
-                                        </div>
-                                    </div>
+                        </div>
+                    </div>
+                    <div className='col-12 col-lg-5  d-flex flex-column justify-content-between'>
+                        <div className='row'>
+                            <div className='col-12 d-flex flex-column justify-content-between'>
+                                <div className='popular_card card_bg_TR card_size-3 mb-card-country' style={{backgroundImage: `url(${turkey})`}}>
+                                    <div className="title_popular_card">Турция</div>
+                                    <div className="description_popular_card">36 гостиницы</div>
                                 </div>
-                                <div className='row'>
-                                    <div className=''></div>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col-12 d-flex flex-column justify-content-between'>
+                                <div className='popular_card card_bg_do card_size-3' style={{backgroundImage: `url(${dominica})`}}>
+                                    <div className='title_popular_card'>Доминиканская республика</div>
+                                    <div className='description_popular_card'>12 гостинницы</div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className='col-12 col-lg-5  d-flex flex-column justify-content-between'></div>
                 </div>
             </div>
         </div>
