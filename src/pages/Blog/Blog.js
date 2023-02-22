5fea6f6aeddfc0d4301a05185913dd5e9d081801
Konstantin.Kolesnik <kostya_kol333@mail.ru>
2023-02-22 11:10:02 +0300

    blog page start

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Header from "../../componets/Header";
+
+import './Blog.scss'
 
 const Blog = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className='bg-gray-wrapper'>
+                <Header/>
+            </div>
+            <main>
+                <div className='blog-page'>
+                    <div className='container-xxl'>
+                        <div className='row'>
+                            <div className='col-12'>
+                                <h1 className='block_title center_title blog_title'>Советы для твоего отдыха от экспертов по путешествиям</h1>
+                                <div className='block_description blog_desc'>Получите вдохновение для следующей поездки.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
     );
 };
 
