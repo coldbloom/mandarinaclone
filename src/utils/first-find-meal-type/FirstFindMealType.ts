517fc9e1db41c687c6102e0549dc8832a203604b
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-03-09 19:10:59 +0300

    add fix

--- src/utils/first-find-meal-type/FirstFindMealType.ts
+++ src/utils/first-find-meal-type/FirstFindMealType.ts
@@ -3,24 +3,30 @@ import { ApiData } from '@/api/apiData/api.data'
 
 const FindTypeInCode = (key:any)=>{
   for(let i = 0;i<ApiData.nutritionType.length;i++){
-    if(ApiData.nutritionType[i].code === key) return ApiData.nutritionType[i].name
+    
+    
+    if(ApiData.nutritionType[i].code === key) {
+      console.log(key,'wfewef');
+     return ApiData.nutritionType[i].name
+    } 
   }
 }
 
 export const FirstFindMealType = (meal_types: any) => {
-	console.log(ApiData.nutritionType, meal_types)
   let state = []
-  let firstKey = 0
+  let firstKey = -1
 	if (!meal_types) return ''
 	for (let i = 0; i < ApiData.nutritionType.length; i++) {
     const key = meal_types.indexOf(ApiData.nutritionType[i].code)
     if(key !== -1){
+      console.log(firstKey);
       state.push(ApiData.nutritionType[i].code)
-      if(!firstKey) firstKey = key
+      if(firstKey === -1) firstKey = key
     }
-  }
 
+  }
+  if(firstKey === -1) return 'Выберите параметры';
   if(state.length>1 ) return `${FindTypeInCode(meal_types[firstKey])} и лучше`;
-  if(state.length ===1) return `${FindTypeInCode(meal_types[firstKey])}`
-	return 'Выберите парамертры'
+  if(state.length===1) return `${FindTypeInCode(meal_types[firstKey])}`;
+  return 'Выберите параметры'
 }
